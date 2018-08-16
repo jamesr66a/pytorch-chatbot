08e528d23e6589dcf34a742ffc62bb7ec3b2b967
James Reed <jamesreed@fb.com>
2018-08-16 16:21:30 -0400

    MILSTM

--- refactored_seq2seq.py
+++ refactored_seq2seq.py
@@ -141,16 +141,16 @@ for attn_type in ['general', 'concat', 'dot']:
     hidden_t_traced = fake_decoder_hidden.clone()
 
 
-    # n_timesteps = 20
-    # for i in range(n_timesteps):
-    #     print(input_t_pytorch[0, :], input_t_traced[0, :])
-    #     scores_t_pytorch, hidden_t_pytorch = pytorch_decoder(input_t_pytorch, hidden_t_pytorch, traced_encoder_out)
-    #     scores_t_traced, hidden_t_traced = traced_decoder(input_t_traced, hidden_t_traced, traced_encoder_out)
-    #     np.testing.assert_allclose(input_t_pytorch.detach().numpy(), input_t_traced.detach().numpy())
-    #     np.testing.assert_allclose(hidden_t_pytorch.detach().numpy(), hidden_t_traced.detach().numpy())
-    #
-    #     input_t_pytorch = torch.argmax(scores_t_pytorch, dim=2)
-    #     input_t_traced = torch.argmax(scores_t_traced, dim=2)
+    n_timesteps = 20
+    for i in range(n_timesteps):
+        print(input_t_pytorch[0, :], input_t_traced[0, :])
+        scores_t_pytorch, hidden_t_pytorch = pytorch_decoder(input_t_pytorch, hidden_t_pytorch, traced_encoder_out)
+        scores_t_traced, hidden_t_traced = traced_decoder(input_t_traced, hidden_t_traced, traced_encoder_out)
+        np.testing.assert_allclose(input_t_pytorch.detach().numpy(), input_t_traced.detach().numpy())
+        np.testing.assert_allclose(hidden_t_pytorch.detach().numpy(), hidden_t_traced.detach().numpy())
+
+        input_t_pytorch = torch.argmax(scores_t_pytorch, dim=2)
+        input_t_traced = torch.argmax(scores_t_traced, dim=2)
 
 
 
@@ -170,20 +170,22 @@ class GreedySearchDecoder(torch.jit.ScriptModule):
     __constants__ = ['C']
 
     @torch.jit.script_method
-    def forward(self, input_tokens, input_seq_lens, n_timesteps, input_t, all_tokens):
-        # type: (Tensor, Tensor, int, Tensor, Tensor) -> Tensor
+    def forward(self, input_tokens, input_seq_lens, n_timesteps, input_t, all_tokens, all_scores):
+        # type: (Tensor, Tensor, int, Tensor, Tensor, Tensor) -> Tuple[Tensor, Tensor]
         enc_outs, enc_hiddens = self.encoder(input_tokens, input_seq_lens)
+        # TODO: fix the lack of type casts
         # input_t = torch.zeros([1, input_tokens.size(1)])
         hidden_t = torch.zeros([1, input_tokens.size(1), self.C])
 
         # all_tokens = torch.zeros([0, input_tokens.size(1)])
         for i in range(n_timesteps):
-            scores_t, hidden_t = self.decoder(input_t, hidden_t, enc_outs)
-            input_t = torch.argmax(scores_t, dim=2)
+            prob_t, hidden_t = self.decoder(input_t, hidden_t, enc_outs)
+            scores_t, input_t = torch.max(prob_t, dim=2)
 
             all_tokens = torch.cat((all_tokens, input_t), dim=0)
+            all_scores = torch.cat((all_scores, scores_t), dim=0)
 
-        return all_tokens
+        return all_tokens, all_scores
 
 search = GreedySearchDecoder(T=20, B=5, C=512, n_words=10000)
 print(search.graph)
@@ -192,5 +194,162 @@ test_seq_len = torch.LongTensor(-np.sort(-np.random.randint(low=1, high=20, size
 
 input_t = torch.zeros([1, test_inputs.size(1)]).long()
 all_tokens = torch.zeros([0, test_inputs.size(1)]).long()
+all_scores = torch.zeros([0, test_inputs.size(1)])
 
-print(search(test_inputs, test_seq_len, 20, input_t, all_tokens))
+print(search(test_inputs, test_seq_len, 20, input_t, all_tokens, all_scores))
+
+import math
+
+class MILSTMCell(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(MILSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.weight_ih = torch.nn.Parameter(torch.Tensor(4 * hidden_size, input_size))
+        self.weight_hh = torch.nn.Parameter(torch.Tensor(4 * hidden_size, hidden_size))
+        if bias:
+            self.bias = torch.nn.Parameter(torch.Tensor(4 * hidden_size))
+        else:
+            self.register_parameter("bias", None)
+        self.alpha = torch.nn.Parameter(torch.Tensor(4 * hidden_size))
+        self.beta_h = torch.nn.Parameter(torch.Tensor(4 * hidden_size))
+        self.beta_i = torch.nn.Parameter(torch.Tensor(4 * hidden_size))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, x, hx, cx):
+        # get prev_t, cell_t from states
+        Wx = F.linear(x, self.weight_ih)
+        Uz = F.linear(hx, self.weight_hh)
+
+        # Section 2.1 in https://arxiv.org/pdf/1606.06630.pdf
+        gates = self.alpha * Wx * Uz + self.beta_i * Wx + self.beta_h * Uz + self.bias
+
+        # Same as LSTMCell after this point
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 2)
+
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
+
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * F.tanh(cy)
+
+        return hy, cy
+
+
+class MILSTMEncoder(torch.jit.ScriptModule):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0.0):
+        super(MILSTMEncoder, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+        self.initial_hidden = torch.nn.Parameter(torch.randn(hidden_size))
+        self.initial_cell = torch.nn.Parameter(torch.randn(hidden_size))
+
+        self.milstm = torch.jit.trace(torch.rand(1, 5, hidden_size), self.initial_hidden.repeat(1, 5, 1), self.initial_cell.repeat(1, 5, 1))(MILSTMCell(hidden_size, hidden_size))
+
+    @torch.jit.script_method
+    def forward(self, input, seq_lens):
+        input = self.embedding(input)
+        hidden = self.initial_hidden.unsqueeze(0).repeat([1, input.size(1), 1])
+        cell = self.initial_cell.unsqueeze(0).repeat([1, input.size(1), 1])
+        outputs = torch.zeros([0, input.size(1), input.size(2)])
+        for i in range(input.size(0)):
+            hidden, cell = self.milstm(input[i], hidden, cell)
+            outputs = torch.cat((outputs, hidden), dim=0)
+
+        return outputs, hidden
+
+milstm = MILSTMEncoder(C, embedding)
+
+inputs = torch.LongTensor(20, 5).random_(0, 10000)
+seq_lens = torch.LongTensor(-np.sort(-np.random.randint(low=1, high=20, size=(5,))))
+outs = milstm(inputs, seq_lens)
+print(milstm.graph)
+
+
+class AttentionDecoderMILSTM(torch.nn.Module):
+    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(AttentionDecoderMILSTM, self).__init__()
+
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        self.embedding = embedding
+        self.embedding_dropout = torch.nn.Dropout(dropout)
+        self.milstm = torch.jit.trace(torch.rand(1, 5, hidden_size), torch.rand(1, 5, hidden_size), torch.rand(1, 5, hidden_size))(MILSTMCell(hidden_size, hidden_size))
+        self.concat = torch.nn.Linear(hidden_size * 2, hidden_size)
+        self.out = torch.nn.Linear(hidden_size, output_size)
+
+        self.attn = Attention(attn_model, hidden_size)
+
+    def forward(self, input_one_step, last_hidden, last_cell, encoder_outputs):
+        tokens_embedded = self.embedding(input_one_step)
+        tokens_embedded = self.embedding_dropout(tokens_embedded)
+
+        rnn_output, cell = self.milstm(tokens_embedded, last_hidden, last_cell)
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        context = attn_weights.transpose(0, 1).transpose(1, 2).bmm(encoder_outputs.transpose(0, 1))
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = torch.tanh(self.concat(concat_input))
+        output = self.out(concat_output)
+        output = F.softmax(output, dim=1).unsqueeze(0)
+        return output, rnn_output, cell
+
+class MILSTMGreedySearchDecoder(torch.jit.ScriptModule):
+    def __init__(self, T, B, C, n_words):
+        super(MILSTMGreedySearchDecoder, self).__init__()
+        test_inputs = torch.LongTensor(T, B).random_(0, n_words)
+        test_seq_len = torch.LongTensor(-np.sort(-np.random.randint(low=1, high=T, size=(B,))))
+        self.encoder_embed = torch.nn.Embedding(n_words, C)
+        self.encoder = MILSTMEncoder(C, self.encoder_embed)
+        test_outputs, test_hidden = self.encoder(test_inputs, test_seq_len)
+        input_one_step = torch.LongTensor(1, B).random_(0, n_words)
+        fake_decoder_hidden = torch.randn(1, B, C)
+        self.decoder = torch.jit.trace(input_one_step, fake_decoder_hidden, fake_decoder_hidden, traced_encoder_out)(AttentionDecoderMILSTM(attn_type, decoder_embedding, C, n_words))
+        self.C = C
+
+    __constants__ = ['C']
+
+    @torch.jit.script_method
+    def forward(self, input_tokens, input_seq_lens, n_timesteps, input_t, all_tokens, all_scores):
+        # type: (Tensor, Tensor, int, Tensor, Tensor, Tensor) -> Tuple[Tensor, Tensor]
+        enc_outs, enc_hiddens = self.encoder(input_tokens, input_seq_lens)
+        # TODO: fix the lack of type casts
+        # input_t = torch.zeros([1, input_tokens.size(1)])
+        hidden_t = torch.zeros([1, input_tokens.size(1), self.C])
+        cell_t = torch.zeros([1, input_tokens.size(1), self.C])
+
+        # all_tokens = torch.zeros([0, input_tokens.size(1)])
+        for i in range(n_timesteps):
+            prob_t, hidden_t, cell_t = self.decoder(input_t, hidden_t, cell_t, enc_outs)
+            scores_t, input_t = torch.max(prob_t, dim=2)
+
+            all_tokens = torch.cat((all_tokens, input_t), dim=0)
+            all_scores = torch.cat((all_scores, scores_t), dim=0)
+
+        return all_tokens, all_scores
+
+
+milstm_search = MILSTMGreedySearchDecoder(T=20, B=5, C=512, n_words=10000)
+print(milstm_search.graph)
+test_inputs = torch.LongTensor(20, 5).random_(0, 10000)
+test_seq_len = torch.LongTensor(-np.sort(-np.random.randint(low=1, high=20, size=(B,))))
+
+input_t = torch.zeros([test_inputs.size(1)]).long()
+all_tokens = torch.zeros([0, test_inputs.size(1)]).long()
+all_scores = torch.zeros([0, test_inputs.size(1)])
+
+print(milstm_search(test_inputs, test_seq_len, 20, input_t, all_tokens, all_scores))
